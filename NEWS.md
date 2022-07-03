f39550cf844a1268844638ddcbe3a79d48424e4a
John Niehaus <jniehaus2257@gmail.com>
2022-07-03 17:30:02 +0000

    version 1.1.0

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,20 @@
 # News
 
+# bizicount 1.0.0.9000 (dev)
+
+## Minor
+* Fixed improper univariate distribution aliasing in documentation (#1)
+* Added more badges to readme.md (#4)
+* Pointed BugReports link to correct GitHub location (#5)
+* Added new test to prevent #6 from happening again. 
+
+## Major
+* Fixed fatal error that occurred when the first marginal distribution was zero-inflated,
+ while second margin was not (#6). 
+* Fixed error when trying to use NumDeriv to get hessian if NLM hessian failed (#8).
+* Added a parameter to `bizicount()` that scales continuous covariates automatically
+for users (#7). 
+
 # bizicount 1.0.0
 
 The first release of the package, support for Gaussian and Frank copulas,
--- R/bizicount-methods.R
+++ R/bizicount-methods.R
@@ -274,7 +274,7 @@ extract.bizicount = function(model, CI = NULL, id = TRUE){
 #' @slot z.nid z-scores without margin IDs
 #' @slot p p-values for parameter estimates
 #' @slot p.nid p-values without margin IDs
-#' @slot coefmats A list containing coeficient matrices for each margin
+#' @slot coefmats A list containing coefficient matrices for each margin
 #' @slot loglik Scalar log-likelihood at convergence
 #' @slot grad Gradient vector at convergence
 #' @slot n.iter Number of quasi-newton fitting iterations.
@@ -292,6 +292,8 @@ extract.bizicount = function(model, CI = NULL, id = TRUE){
 #' @slot starts list of starting values used
 #' @slot call The model's call
 #' @slot model List containing model matrices, or `NULL` if `keep = F`.
+#' @slot scaled List indicating which covariates of each margin were scaled
+#' according to the `scaling` parameter.
 #' @export
 setClass("bizicount",
          representation(coef = "numeric",
@@ -322,7 +324,8 @@ setClass("bizicount",
                         cop = "character",
                         starts = "list",
                         call = "language",
-                        model = "ANY"
+                        model = "ANY",
+                        scaled = "character"
          )
 )
 
